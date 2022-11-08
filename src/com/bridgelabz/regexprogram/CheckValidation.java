2c214f50e79a044b53e92900b573b23a60fd05f8
arc-015 <chavanavinash015@gmail.com>
2022-11-08 23:18:56 +0530

    Added User registration in regex program

---
+++ src/com/bridgelabz/regexprogram/CheckValidation.java
@@ -0,0 +1,6 @@
+package com.bridgelabz.regexprogram;
+
+public class CheckValidation {
+
+
+}
---
+++ src/com/bridgelabz/regexprogram/UserRegistration.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.regexprogram;
+
+public class UserRegistration {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the User Registration System needs to ensure all validations are in place during the User Entry");
+
+
+    }
+}
